f8028a8a4ac1863947d2935693f32457e213c25e
Christian Pontesegger <christian.pontesegger@web.de>
2014-11-19 12:37:35 +0100

    removed obsolete scriptUIGraph code
      legacy code was not used actively.
    
    Change-Id: I6d078bcb4e24d82fdfe2b8e66d9498e1be0d54f7

--- plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/scripts/repository/impl/LocationHelper.java
+++ plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/scripts/repository/impl/LocationHelper.java
@@ -7,32 +7,33 @@
  *
  * Contributors:
  *     Christian Pontesegger - initial API and implementation
- *******************************************************************************/package org.eclipse.ease.ui.scripts.repository.impl;
+ *******************************************************************************/
+package org.eclipse.ease.ui.scripts.repository.impl;
 
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
+ import org.eclipse.core.runtime.IConfigurationElement;
+ import org.eclipse.core.runtime.Platform;
 
-public class LocationHelper {
+ public class LocationHelper {
 
-	public static String toLocation(String value) {
-		if ("main".equalsIgnoreCase(value)) {
-			// main toolbar
-			return null;
-		}
+	 public static String toLocation(final String value) {
+		 if ("main".equalsIgnoreCase(value)) {
+			 // main toolbar
+			 return null;
+		 }
 
-		final IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor("org.eclipse.ui.views");
-		for (final IConfigurationElement e : config) {
-			if ("view".equals(e.getName())) {
-				String id = e.getAttribute("id");
-				if (id.equals(value))
-					return value;
+		 final IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor("org.eclipse.ui.views");
+		 for (final IConfigurationElement e : config) {
+			 if ("view".equals(e.getName())) {
+				 String id = e.getAttribute("id");
+				 if (id.equals(value))
+					 return value;
 
-				String name = e.getAttribute("name");
-				if (name.equals(value))
-					return id;
-			}
-		}
+				 String name = e.getAttribute("name");
+				 if (name.equals(value))
+					 return id;
+			 }
+		 }
 
-		return null;
-	}
-}
+		 return null;
+	 }
+ }
