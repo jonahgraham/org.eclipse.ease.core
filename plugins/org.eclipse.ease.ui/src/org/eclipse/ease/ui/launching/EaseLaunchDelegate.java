d0b4ad66f65dab411f9302bc3e644140b805e807
Christian Pontesegger <christian.pontesegger@web.de>
2015-01-12 10:00:52 +0100

    extracted an abstract launch delegate for scripting
      to reuse parts of the launch delegate, generic methods were moved to
      an abstract launcher class. First reuse will be done from the unittest
      framework.
    
    Change-Id: If4079d6d2628ce3086ae818ad05238aba1ba8dd1

--- plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/launching/EaseLaunchDelegate.java
+++ plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/launching/EaseLaunchDelegate.java
@@ -10,15 +10,12 @@
  *******************************************************************************/
 package org.eclipse.ease.ui.launching;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
@@ -26,237 +23,151 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
-import org.eclipse.debug.ui.ILaunchShortcut;
-import org.eclipse.debug.ui.ILaunchShortcut2;
 import org.eclipse.ease.IDebugEngine;
 import org.eclipse.ease.IScriptEngine;
 import org.eclipse.ease.service.EngineDescription;
 import org.eclipse.ease.service.IScriptService;
+import org.eclipse.ease.service.ScriptType;
 import org.eclipse.ease.tools.ResourceTools;
+import org.eclipse.ease.tools.RunnableWithResult;
 import org.eclipse.ease.ui.console.ScriptConsole;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.ease.ui.tools.AbstractLaunchDelegate;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 
 /**
  * Quick launcher for EASE script files.
  */
-public class EaseLaunchDelegate implements ILaunchShortcut, ILaunchShortcut2, ILaunchConfigurationDelegate {
+public class EaseLaunchDelegate extends AbstractLaunchDelegate {
 
 	private static final String LAUNCH_CONFIGURATION_ID = "org.eclipse.ease.launchConfigurationType";
 
-	// **********************************************************************
-	// ILaunchShortcut
-	// **********************************************************************
-
 	@Override
-	public final void launch(final IEditorPart editor, final String mode) {
-		launch(getLaunchableResource(editor), mode);
-	}
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 
-	@Override
-	public final void launch(final ISelection selection, final String mode) {
-		launch(getLaunchableResource(selection), mode);
-	}
+		final Object resource = ResourceTools.resolveFile(getFileLocation(configuration), null, true);
 
-	// **********************************************************************
-	// ILaunchShortcut2
-	// **********************************************************************
+		// create engine
+		final String engineID = configuration.getAttribute(LaunchConstants.SCRIPT_ENGINE, "");
+		final IScriptService scriptService = (IScriptService) PlatformUI.getWorkbench().getService(IScriptService.class);
+		EngineDescription engineDescription = scriptService.getEngineByID(engineID);
+		if ((ILaunchManager.DEBUG_MODE.equals(mode)) && (!engineDescription.supportsDebugging())) {
+			// we are trying to debug using an engine that does not support debugging
+			engineDescription = null;
 
-	@Override
-	public final IResource getLaunchableResource(final IEditorPart editorpart) {
-		final IEditorInput input = editorpart.getEditorInput();
-		if (input instanceof FileEditorInput)
-			return ((FileEditorInput) input).getFile();
+			// try to find an engine that supports debugging
+			final ScriptType scriptType = scriptService.getScriptType(ResourceTools.toAbsoluteLocation(resource, null));
+			final List<EngineDescription> engines = scriptService.getEngines(scriptType.getName());
+			for (final EngineDescription description : engines) {
+				if (description.supportsDebugging()) {
+					// matching debug engine found
+					engineDescription = description;
+					break;
+				}
+			}
 
-		return null;
-	}
+			if (engineDescription != null) {
+				// ask user if he wants to change the engine: once, permanently or not
+				final RunnableWithResult<Boolean> runnable = new RunnableWithResult<Boolean>() {
+					@Override
+					public void run() {
+						final boolean confirmEngineSwitch = MessageDialog
+								.openConfirm(
+										Display.getDefault().getActiveShell(),
+										"Configuration change needed",
+										"The currently selected script engine does not support debugging. However an alternative engine is available. Do you want to debug your script using that alternative engine?");
 
-	@Override
-	public final IResource getLaunchableResource(final ISelection selection) {
-		if (selection instanceof IStructuredSelection) {
-			for (final Object element : ((IStructuredSelection) selection).toArray()) {
-				if (element instanceof IFile)
-					return (IResource) element;
+						setResult(confirmEngineSwitch);
+					}
+				};
+
+				Display.getDefault().syncExec(runnable);
+
+				if (!runnable.getResult())
+					// user does not want to switch engine
+					return;
+
+			} else {
+				// giving up
+				Display.getDefault().asyncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						MessageDialog.openError(Display.getDefault().getActiveShell(), "Launch error", "No debug engine available for \"" + resource + "\"");
+					}
+				});
+				return;
 			}
 		}
 
-		return null;
-	}
+		final IScriptEngine engine = engineDescription.createEngine();
+		engine.setTerminateOnIdle(true);
 
-	@Override
-	public final ILaunchConfiguration[] getLaunchConfigurations(final IEditorPart editorpart) {
-		return getLaunchConfgurations(getLaunchableResource(editorpart), ILaunchManager.RUN_MODE);
-	}
+		// initialize console
+		final ScriptConsole console = ScriptConsole.create(engine.getName() + ": " + resource, engine);
+		engine.setOutputStream(console.getOutputStream());
+		engine.setErrorStream(console.getErrorStream());
 
-	@Override
-	public final ILaunchConfiguration[] getLaunchConfigurations(final ISelection selection) {
-		return getLaunchConfgurations(getLaunchableResource(selection), ILaunchManager.RUN_MODE);
-	}
+		// setup debugger
+		if (ILaunchManager.DEBUG_MODE.equals(mode)) {
 
-	// **********************************************************************
-	// ILaunchConfigurationDelegate
-	// **********************************************************************
-
-	@Override
-	public void launch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch, final IProgressMonitor monitor) throws CoreException {
-		Object resource = ResourceTools.getResource(configuration.getAttribute(LaunchConstants.FILE_LOCATION, ""));
-		;
-		if (resource != null) {
-			// we have a valid script, lets feed it to the script engine
-			launch(resource, configuration, mode, launch, monitor);
+			setupDebugger(engine, configuration, launch);
 		}
+
+		// set startup parameters
+		final String parameterString = configuration.getAttribute(LaunchConstants.STARTUP_PARAMETERS, "").trim();
+		String[] parameters;
+		if (!parameterString.isEmpty()) {
+			parameters = parameterString.split("\\s+");
+
+		} else
+			parameters = new String[0];
+
+		engine.setVariable("argv", parameters);
+
+		// execute resource
+		engine.executeAsync(resource);
+
+		// start engine
+		engine.schedule();
 	}
 
-	// **********************************************************************
-	// internal stuff
-	// **********************************************************************
-
-	/**
-	 * Get all launch configurations that target a dedicated resource file.
-	 *
-	 * @param resource
-	 *            root file to execute
-	 * @param mode
-	 * @return {@link ILaunchConfiguration}s using resource
-	 */
-	private ILaunchConfiguration[] getLaunchConfgurations(final IResource resource, final String mode) {
-		final List<ILaunchConfiguration> configurations = new ArrayList<ILaunchConfiguration>();
-
+	@Override
+	protected ILaunchConfiguration createLaunchConfiguration(IResource file, String mode) throws CoreException {
 		final ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
 		final ILaunchConfigurationType type = manager.getLaunchConfigurationType(LAUNCH_CONFIGURATION_ID);
 
-		// try to find existing configurations using the same file
-		String resourceLocation = ResourceTools.toAbsoluteLocation(resource, null);
-		try {
-			for (final ILaunchConfiguration configuration : manager.getLaunchConfigurations(type)) {
-				try {
-					String configurationUri = configuration.getAttribute(LaunchConstants.FILE_LOCATION, "");
-					if (resourceLocation.equals(configurationUri)) {
-						// we have a candidate
-						if (ILaunchManager.DEBUG_MODE.equals(mode)) {
-							String engineID = configuration.getAttribute(LaunchConstants.SCRIPT_ENGINE, "");
-							final IScriptService scriptService = (IScriptService) PlatformUI.getWorkbench().getService(IScriptService.class);
-							EngineDescription engineDescription = scriptService.getEngineByID(engineID);
-							if (engineDescription.supportsDebugging())
-								configurations.add(configuration);
+		final ILaunchConfigurationWorkingCopy configuration = type.newInstance(null, file.getName());
+		configuration.setAttribute(LaunchConstants.FILE_LOCATION, ResourceTools.toAbsoluteLocation(file, null));
 
-						} else
-							configurations.add(configuration);
-					}
+		// find a valid engine
+		final IScriptService scriptService = (IScriptService) PlatformUI.getWorkbench().getService(IScriptService.class);
+		final Collection<EngineDescription> engines = scriptService.getScriptType(ResourceTools.toAbsoluteLocation(file, null)).getEngines();
+		if (engines.isEmpty())
+			// TODO use a better way to bail out and use the direct file launch
+			throw new CoreException(Status.CANCEL_STATUS);
 
-				} catch (final CoreException e) {
-					// could not read configuration, ignore
-				}
-			}
-		} catch (final CoreException e) {
-			// could not load configurations, ignore
-		}
+		configuration.setAttribute(LaunchConstants.SCRIPT_ENGINE, engines.iterator().next().getID());
 
-		return configurations.toArray(new ILaunchConfiguration[configurations.size()]);
+		// by default a debug configuration should suspend on script startup
+		if (ILaunchManager.DEBUG_MODE.equals(mode))
+			configuration.setAttribute(LaunchConstants.SUSPEND_ON_STARTUP, true);
+
+		// save and return new configuration
+		configuration.doSave();
+
+		return configuration;
 	}
 
-	/**
-	 * Launch a resource. Try to launch using a launch configuration. Used for contextual launches
-	 *
-	 * @param file
-	 *            source file
-	 * @param mode
-	 *            launch mode
-	 */
-	private void launch(final IResource file, final String mode) {
-
-		if (file instanceof IFile) {
-			// try to save dirty editors
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().saveAllEditors(true);
-
-			try {
-				ILaunchConfiguration[] configurations = getLaunchConfgurations(file, mode);
-
-				if (configurations.length == 0) {
-					// no configuration found, create new one
-					final ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-					final ILaunchConfigurationType type = manager.getLaunchConfigurationType(LAUNCH_CONFIGURATION_ID);
-
-					final ILaunchConfigurationWorkingCopy configuration = type.newInstance(null, file.getName());
-					configuration.setAttribute(LaunchConstants.FILE_LOCATION, ResourceTools.toAbsoluteLocation(file, null));
-
-					// find a valid engine
-					final IScriptService scriptService = (IScriptService) PlatformUI.getWorkbench().getService(IScriptService.class);
-					Collection<EngineDescription> engines = scriptService.getScriptType(ResourceTools.toAbsoluteLocation(file, null)).getEngines();
-					if (engines.isEmpty())
-						// TODO use a better way to bail out and use the direct file launch
-						throw new CoreException(Status.CANCEL_STATUS);
-
-					configuration.setAttribute(LaunchConstants.SCRIPT_ENGINE, engines.iterator().next().getID());
-
-					// save and return new configuration
-					configuration.doSave();
-
-					configurations = new ILaunchConfiguration[] { configuration };
-				}
-
-				// launch
-				configurations[0].launch(mode, new NullProgressMonitor());
-
-			} catch (final CoreException e) {
-				// could not create launch configuration, run file directly
-				launch(file, null, mode, null, new NullProgressMonitor());
-			}
-		}
+	@Override
+	protected String getFileLocation(ILaunchConfiguration configuration) throws CoreException {
+		return configuration.getAttribute(LaunchConstants.FILE_LOCATION, "");
 	}
 
-	/**
-	 * Execute script code from an {@link IFile}.
-	 *
-	 * @param resource
-	 *            resource to execute
-	 * @param configuration
-	 *            launch configuration
-	 * @param mode
-	 *            launch mode
-	 * @param launch
-	 * @param monitor
-	 */
-	private void launch(final Object resource, final ILaunchConfiguration configuration, final String mode, final ILaunch launch, final IProgressMonitor monitor) {
-
-		try {
-			String engineID = configuration.getAttribute(LaunchConstants.SCRIPT_ENGINE, "");
-			final IScriptService scriptService = (IScriptService) PlatformUI.getWorkbench().getService(IScriptService.class);
-			final IScriptEngine engine = scriptService.getEngineByID(engineID).createEngine();
-
-			final ScriptConsole console = ScriptConsole.create(engine.getName() + ": " + resource, engine);
-			engine.setOutputStream(console.getOutputStream());
-			engine.setErrorStream(console.getErrorStream());
-
-			engine.setTerminateOnIdle(true);
-
-			if (ILaunchManager.DEBUG_MODE.equals(mode))
-				setupDebugger(engine, configuration, launch);
-
-			// set startup parameters
-			String parameterString = configuration.getAttribute(LaunchConstants.STARTUP_PARAMETERS, "").trim();
-			String[] parameters;
-			if (!parameterString.isEmpty()) {
-				parameters = parameterString.split("\\s+");
-
-			} else
-				parameters = new String[0];
-
-			engine.setVariable("argv", parameters);
-
-			engine.executeAsync(resource);
-
-			engine.schedule();
-
-		} catch (CoreException e) {
-			// TODO handle this exception (but for now, at least know it happened)
-			throw new RuntimeException(e);
-		}
+	@Override
+	protected String getLaunchConfigurationId() {
+		return LAUNCH_CONFIGURATION_ID;
 	}
 
 	private void setupDebugger(final IScriptEngine engine, final ILaunchConfiguration configuration, final ILaunch launch) {
@@ -264,19 +175,19 @@ public class EaseLaunchDelegate implements ILaunchShortcut, ILaunchShortcut2, IL
 			boolean suspendOnStartup = false;
 			try {
 				suspendOnStartup = configuration.getAttribute(LaunchConstants.SUSPEND_ON_STARTUP, false);
-			} catch (CoreException e) {
+			} catch (final CoreException e) {
 			}
 
 			boolean suspendOnScriptLoad = false;
 			try {
 				suspendOnScriptLoad = configuration.getAttribute(LaunchConstants.SUSPEND_ON_SCRIPT_LOAD, false);
-			} catch (CoreException e) {
+			} catch (final CoreException e) {
 			}
 
 			boolean showDynamicCode = false;
 			try {
 				showDynamicCode = configuration.getAttribute(LaunchConstants.DISPLAY_DYNAMIC_CODE, false);
-			} catch (CoreException e) {
+			} catch (final CoreException e) {
 			}
 
 			((IDebugEngine) engine).setupDebugger(launch, suspendOnStartup, suspendOnScriptLoad, showDynamicCode);
---
+++ plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/tools/AbstractLaunchDelegate.java
@@ -0,0 +1,166 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Christian Pontesegger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christian Pontesegger - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ease.ui.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.debug.ui.ILaunchShortcut2;
+import org.eclipse.ease.Logger;
+import org.eclipse.ease.tools.ResourceTools;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+
+/**
+ * Quick launcher for EASE script files.
+ */
+public abstract class AbstractLaunchDelegate implements ILaunchShortcut, ILaunchShortcut2, ILaunchConfigurationDelegate {
+
+	// **********************************************************************
+	// ILaunchShortcut
+	// **********************************************************************
+
+	@Override
+	public final void launch(final IEditorPart editor, final String mode) {
+		launch(getLaunchableResource(editor), mode);
+	}
+
+	@Override
+	public final void launch(final ISelection selection, final String mode) {
+		launch(getLaunchableResource(selection), mode);
+	}
+
+	// **********************************************************************
+	// ILaunchShortcut2
+	// **********************************************************************
+
+	@Override
+	public final IResource getLaunchableResource(final IEditorPart editorpart) {
+		final IEditorInput input = editorpart.getEditorInput();
+		if (input instanceof FileEditorInput)
+			return ((FileEditorInput) input).getFile();
+
+		return null;
+	}
+
+	@Override
+	public final IResource getLaunchableResource(final ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			for (final Object element : ((IStructuredSelection) selection).toArray()) {
+				if (element instanceof IFile)
+					return (IResource) element;
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public final ILaunchConfiguration[] getLaunchConfigurations(final IEditorPart editorpart) {
+		return getLaunchConfigurations(getLaunchableResource(editorpart), ILaunchManager.RUN_MODE);
+	}
+
+	@Override
+	public final ILaunchConfiguration[] getLaunchConfigurations(final ISelection selection) {
+		return getLaunchConfigurations(getLaunchableResource(selection), ILaunchManager.RUN_MODE);
+	}
+
+	// **********************************************************************
+	// internal stuff
+	// **********************************************************************
+
+	/**
+	 * Get all launch configurations that target a dedicated resource file.
+	 *
+	 * @param resource
+	 *            root file to execute
+	 * @param mode
+	 * @return {@link ILaunchConfiguration}s using resource
+	 */
+	protected ILaunchConfiguration[] getLaunchConfigurations(final IResource resource, final String mode) {
+		final List<ILaunchConfiguration> configurations = new ArrayList<ILaunchConfiguration>();
+
+		final ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+		final ILaunchConfigurationType type = manager.getLaunchConfigurationType(getLaunchConfigurationId());
+
+		// try to find existing configurations using the same file
+		final String resourceLocation = ResourceTools.toAbsoluteLocation(resource, null);
+		try {
+			for (final ILaunchConfiguration configuration : manager.getLaunchConfigurations(type)) {
+				final String configurationUri = getFileLocation(configuration);
+				if (resourceLocation.equals(configurationUri)) {
+					// we have a candidate
+					configurations.add(configuration);
+				}
+			}
+		} catch (final CoreException e) {
+			// could not load configurations, ignore
+		}
+
+		return configurations.toArray(new ILaunchConfiguration[configurations.size()]);
+	}
+
+	/**
+	 * Launch a resource. Try to launch using a launch configuration. Used for contextual launches
+	 *
+	 * @param file
+	 *            source file
+	 * @param mode
+	 *            launch mode
+	 */
+	private void launch(final IResource file, final String mode) {
+
+		if (file instanceof IFile) {
+			// try to save dirty editors
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().saveAllEditors(true);
+
+			try {
+				ILaunchConfiguration[] configurations = getLaunchConfigurations(file, mode);
+
+				if (configurations.length == 0) {
+					final ILaunchConfiguration configuration = createLaunchConfiguration(file, mode);
+
+					configurations = new ILaunchConfiguration[] { configuration };
+				}
+
+				// launch
+				configurations[0].launch(mode, new NullProgressMonitor());
+
+			} catch (final CoreException e) {
+				// could not launch configuration, giving up
+				MessageDialog.openError(Display.getDefault().getActiveShell(), "Launch Error", "Could not launch \"" + file + "\"");
+				Logger.logError("Could not launch \"" + file + "\"", e);
+			}
+		}
+	}
+
+	protected abstract ILaunchConfiguration createLaunchConfiguration(IResource file, String mode) throws CoreException;
+
+	protected abstract String getFileLocation(ILaunchConfiguration configuration) throws CoreException;
+
+	protected abstract String getLaunchConfigurationId();
+}
