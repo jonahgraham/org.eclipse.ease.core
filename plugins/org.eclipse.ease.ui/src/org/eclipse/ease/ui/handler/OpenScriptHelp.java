7ec8c413b278d840f6bd68f59165a97bc93d66ca
Vidura Mudalige <vbmudalige@gmail.com>
2015-03-18 20:33:12 +0530

    [429787] - Add help button to script shell
    Added a OpenScriptShell.java file as the handler class of the button. Also
    created a new html file as the root of scripting guide help.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=429787
    Signed-off-by: Vidura Mudalige <vbmudalige@gmail.com>

---
+++ plugins/org.eclipse.ease.ui/src/org/eclipse/ease/ui/handler/OpenScriptHelp.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Vidura Mudalige and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Vidura Mudalige - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ease.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.PlatformUI;
+
+public class OpenScriptHelp extends AbstractHandler implements IHandler {
+
+	public static final String SCRIPT_GUIDE_URI = "/org.eclipse.ease.help/help/html/scripting_guide.html";
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		PlatformUI.getWorkbench().getHelpSystem().displayHelpResource(SCRIPT_GUIDE_URI);
+		return null;
+	}
+
+}
