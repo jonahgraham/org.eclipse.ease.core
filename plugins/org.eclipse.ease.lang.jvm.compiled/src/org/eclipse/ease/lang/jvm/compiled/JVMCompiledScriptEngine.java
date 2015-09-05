fcd26799f11f17b0c5197e1222baa18aa501bae5
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-09-04 18:30:41 -0700

    Fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=476685
    Change-Id: I35e422ba0788b66e8e32555e18fb7220f3ef3615
    Signed-off-by: Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>

--- plugins/org.eclipse.ease.lang.jvm.compiled/src/org/eclipse/ease/lang/jvm/compiled/JVMCompiledScriptEngine.java
+++ plugins/org.eclipse.ease.lang.jvm.compiled/src/org/eclipse/ease/lang/jvm/compiled/JVMCompiledScriptEngine.java
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -92,9 +94,36 @@ public class JVMCompiledScriptEngine extends AbstractScriptEngine implements ISc
 
 	@Override
 	public String getSaveVariableName(final String name) {
-		throw new RuntimeException("Functionality not supported by this engine");
+		return getSaveName(name);
 	}
 
+	public static String getSaveName(final String identifier) {
+		// check if name is already valid
+		if (isSaveName(identifier))
+			return identifier;
+
+		// not valid, convert string to valid format
+		final StringBuilder buffer = new StringBuilder(identifier.replaceAll("[^a-zA-Z0-9_$]", "_"));
+
+		// check for valid first character
+		if (buffer.length() > 0) {
+			final char start = buffer.charAt(0);
+			if (((start < 65) || ((start > 90) && (start < 97)) || (start > 122)) && (start != '_'))
+				buffer.insert(0, '_');
+		} else {
+			// buffer is empty, create a random string of lowercase letters
+			buffer.append('_');
+			for (int index = 0; index < new Random().nextInt(20); index++)
+				buffer.append('a' + new Random().nextInt(26));
+		}
+
+		return buffer.toString();
+	}
+
+	public static boolean isSaveName(final String identifier) {
+		return Pattern.matches("[a-zA-Z_$][a-zA-Z0-9_$]*", identifier);
+	}
+	
 	@Override
 	public void registerJar(final URL url) {
 		throw new RuntimeException("Functionality not supported by this engine");
