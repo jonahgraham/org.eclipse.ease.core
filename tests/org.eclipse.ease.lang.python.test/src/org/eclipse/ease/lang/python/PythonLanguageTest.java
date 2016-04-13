743d531421b9e3257ec362df20690cf8fb23f747
Jonah Graham <jonah@kichwacoders.com>
2016-04-13 15:56:06 +0100

    Bug 491592: Create org.eclipse.ease.lang.python.test
      The actual tests are part of susbsequent commits.
    
    Change-Id: I802e70f8b9eec1670d67ab752185626361cf8bf9
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

---
+++ tests/org.eclipse.ease.lang.python.test/src/org/eclipse/ease/lang/python/PythonLanguageTest.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Kichwa Coders and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Kichwa Coders - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ease.lang.python;
+
+import org.junit.Test;
+
+public class PythonLanguageTest {
+	// TODO write the real test - test plug-ins need at least one test otherwise they fail
+	// (the other choice is failIfNoTests in pom.xml)
+	@Test
+	public void testTODO() {
+
+	}
+}
