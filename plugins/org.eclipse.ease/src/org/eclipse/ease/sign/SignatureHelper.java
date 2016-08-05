585d0291e2863879aa7ed20af5ee5103a01057e5
Varun Raval <vraval48@gmail.com>
2016-08-05 17:43:50 +0530

    Bug 494102 - Provide libraries for Signature Signing functionality
      This commit removes a method in SignatureHelper and adds a new one.
      
      Instead of directly appending signature to file, signature will be
    converted to proper format and returned.
      
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=494102
    Change-Id: I5f6c1fe414c29726f6f0f244b1bd924342e7b845
    Signed-off-by: Varun Raval <vraval48@gmail.com>

--- plugins/org.eclipse.ease/src/org/eclipse/ease/sign/SignatureHelper.java
+++ plugins/org.eclipse.ease/src/org/eclipse/ease/sign/SignatureHelper.java
@@ -11,17 +11,13 @@
 
 package org.eclipse.ease.sign;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SignatureException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.util.Arrays;
 import java.util.Base64;
 
 import org.eclipse.ease.Activator;
@@ -38,6 +34,12 @@ public class SignatureHelper {
 
 	private static final String BEGIN_STRING = "-----BEGIN SIGNATURE-----", END_STRING = "-----END SIGNATURE-----";
 
+	// set default message digest algorithm
+	public static final String DEFAULT_MESSAGE_DIGEST_ALGO = "SHA256";
+
+	// set default signature provider
+	public static final String DEFAULT_SIGNATURE_PROVIDER = "preferred";
+
 	/**
 	 * Converts given bytes in {@link Base64} form.
 	 *
@@ -50,7 +52,7 @@ public class SignatureHelper {
 		if (bytes == null)
 			return null;
 
-		Base64.Encoder b = Base64.getEncoder();
+		final Base64.Encoder b = Base64.getEncoder();
 		return b.encodeToString(bytes);
 	}
 
@@ -66,14 +68,15 @@ public class SignatureHelper {
 		if (str == null)
 			return null;
 
-		Base64.Decoder decoder = Base64.getDecoder();
+		final Base64.Decoder decoder = Base64.getDecoder();
 		return decoder.decode(str);
 	}
 
 	/**
-	 * Appends given signature, messageSigestAlgorithm, provider, and certificate to given file.<br/>
-	 * Format for appending signature will be as follows:
+	 * Converts given signature, messageSigestAlgorithm, provider, and certificate in proper format.<br/>
+	 * Format for signature block will be as follows:
 	 * <p>
+	 * newline <br/>
 	 * <i>blockCommentStart</i><br/>
 	 * -----BEGIN SIGNATURE-----<br/>
 	 * Hash:SHA1 Provider:SUN <br/>
@@ -83,7 +86,8 @@ public class SignatureHelper {
 	 * certificate chain in {@link Base64} format (multiple lines)(each line containing 64 bytes)<br/>
 	 * <br/>
 	 * -----END SIGNSTURE-----<br/>
-	 * <i>blockCommentEnd</i>
+	 * <i>blockCommentEnd</i><br/>
+	 * newline
 	 * </p>
 	 *
 	 * @param scriptType
@@ -93,84 +97,75 @@ public class SignatureHelper {
 	 * @param certStr
 	 *            string representation of certificate chain in Base64 format
 	 * @param messageDigestAlgo
-	 *            name the message-digest algorithm using which signature is created. Provide <code>null</code> or empty string or 'default' to set 'SHA1'
+	 *            name the message-digest algorithm using which signature is created. Provide <code>null</code> or empty string or 'default' to set default
+	 *            algorithm
 	 * @param provider
 	 *            name the provider used to perform signature. Provide <code>null</code> or empty string to set 'preferred'
-	 * @param dataStream
-	 *            stream to which signature and certificate are to be attached
-	 * @return <code>true</code> if signature is written to dataStream and <code>false</code> if signature can't be written for e.g. due to IOException
+	 * @return string representation of signature block in proper format
 	 * @throws ScriptSignatureException
 	 *             when one or more parameter are <code>null</code> or empty
 	 */
-	public static boolean appendSignature(final ScriptType scriptType, final String signStr, final String certStr, String messageDigestAlgo, String provider,
-			final OutputStream dataStream) throws ScriptSignatureException {
+	public static String getSignatureInFormat(final ScriptType scriptType, final String signStr, final String certStr, String messageDigestAlgo,
+			String provider) throws ScriptSignatureException {
 
-		if (scriptType == null || signStr == null || signStr.isEmpty() || certStr == null || certStr.isEmpty() || dataStream == null)
+		if ((scriptType == null) || (signStr == null) || signStr.isEmpty() || (certStr == null) || certStr.isEmpty())
 			throw new ScriptSignatureException("One or more parameters are null or empty");
 
-		if (messageDigestAlgo == null || messageDigestAlgo.isEmpty() || "default".equalsIgnoreCase(messageDigestAlgo))
-			messageDigestAlgo = "SHA1";
+		if ((messageDigestAlgo == null) || messageDigestAlgo.isEmpty() || "default".equalsIgnoreCase(messageDigestAlgo))
+			messageDigestAlgo = DEFAULT_MESSAGE_DIGEST_ALGO;
 
-		if (provider == null || provider.isEmpty())
-			provider = "preferred";
+		if ((provider == null) || provider.isEmpty())
+			provider = DEFAULT_SIGNATURE_PROVIDER;
 
 		final String begin = "\n" + BEGIN_STRING, end = END_STRING + "\n", signatureParam = "Hash:" + messageDigestAlgo + " Provider:" + provider;
 
-		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(dataStream);
-		try {
-			/*
-			 * By default, last line in every file is ended by \n which is not visible directly. But if generated programmatically, it may not.
-			 *
-			 * A single \n character is added always to start signature block from new line. It may give atmost a single blank line in case \n character is
-			 * already present.
-			 */
-			// TODO remember while appending to file
-			// bufferedOutputStream.write("\n".getBytes());
+		/*
+		 * By default, last line in every file is ended by \n which is not visible directly. But if generated programmatically, it may not.
+		 *
+		 * A single \n character is added always to start signature block from new line. It may give atmost a single blank line in case \n character is already
+		 * present.
+		 */
+		// TODO remember while appending to file
+		// strBuf.append("\n");
 
-			// using createCommentedString method of ICodeFactory to make comment of signature block
+		final StringBuffer strBuf = new StringBuffer();
 
-			StringBuffer strBuf = new StringBuffer();
+		strBuf.append(begin);
+		strBuf.append("\n");
 
-			strBuf.append(begin);
-			strBuf.append("\n");
+		strBuf.append(signatureParam);
+		strBuf.append("\n\n");
 
-			strBuf.append(signatureParam);
-			strBuf.append("\n\n");
+		strBuf.append(signStr);
+		strBuf.append("\n\n");
 
-			strBuf.append(signStr);
-			strBuf.append("\n\n");
-
-			int i = 0;
-			for (String s : certStr.split("")) {
-				strBuf.append(s);
-				i++;
-				if (i % 48 == 0)
-					strBuf.append("\n");
-			}
-
-			if (i % 48 != 0)
+		int i = 0;
+		for (final String s : certStr.split("")) {
+			strBuf.append(s);
+			i++;
+			if ((i % 48) == 0)
 				strBuf.append("\n");
-
-			strBuf.append("\n");
-			strBuf.append(end);
-
-			ICodeFactory iCodeFactory = scriptType.getCodeFactory();
-			bufferedOutputStream.write(iCodeFactory.createCommentedString(strBuf.toString(), true).getBytes());
-			bufferedOutputStream.write("\n".getBytes());
-			return true;
-
-		} catch (IOException e) {
-			Logger.error(Activator.PLUGIN_ID, Arrays.toString(e.getStackTrace()), e);
-
-		} finally {
-			try {
-				if (bufferedOutputStream != null)
-					bufferedOutputStream.close();
-			} catch (IOException e) {
-				Logger.error(Activator.PLUGIN_ID, Arrays.toString(e.getStackTrace()), e);
-			}
 		}
-		return false;
+
+		if ((i % 48) != 0)
+			strBuf.append("\n");
+
+		strBuf.append("\n");
+		strBuf.append(end);
+
+		final ICodeFactory iCodeFactory = scriptType.getCodeFactory();
+
+		final StringBuffer finalBuf = new StringBuffer();
+
+		// appending initial required new line character
+		finalBuf.append("\n");
+
+		// using createCommentedString method of ICodeFactory to make comment of signature block
+		finalBuf.append(iCodeFactory.createCommentedString(strBuf.toString(), true));
+
+		// appending final required new line character
+		finalBuf.append("\n");
+		return finalBuf.toString();
 	}
 
 	/**
@@ -184,9 +179,9 @@ public class SignatureHelper {
 	 * @throws ScriptSignatureException
 	 *             when signature format is improper
 	 */
-	public boolean containSignature(final ScriptType scriptType, final InputStream inputStream) throws ScriptSignatureException {
+	public static boolean containSignature(final ScriptType scriptType, final InputStream inputStream) throws ScriptSignatureException {
 
-		ICodeParser iCodeParser = scriptType.getCodeParser();
+		final ICodeParser iCodeParser = scriptType.getCodeParser();
 		return iCodeParser.getSignatureInfo(inputStream) != null;
 	}
 
@@ -207,19 +202,19 @@ public class SignatureHelper {
 			certificate.verify(certificate.getPublicKey());
 			return true;
 
-		} catch (CertificateException e) {
+		} catch (final CertificateException e) {
 			Logger.error(Activator.PLUGIN_ID, "Error while parsing certificate.", e);
 			throw new ScriptSignatureException("Error while parsing certificate.", e);
-		} catch (InvalidKeyException e) {
+		} catch (final InvalidKeyException e) {
 			throw new ScriptSignatureException("Key of the certificate is invalid.", e);
 
-		} catch (NoSuchAlgorithmException e) {
+		} catch (final NoSuchAlgorithmException e) {
 			throw new ScriptSignatureException("No aprovider support this type of algorithm.", e);
 
-		} catch (NoSuchProviderException e) {
+		} catch (final NoSuchProviderException e) {
 			throw new ScriptSignatureException("No provider for this certificate.", e);
 
-		} catch (SignatureException e) {
+		} catch (final SignatureException e) {
 			// private key with which certificate was signed does not correspond to this public key. Hence it is not self-signed certificate
 			return false;
 		}
